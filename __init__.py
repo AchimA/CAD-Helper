09d39014ea92fcf8562db87b2e094fa892550bd5
AchimA <achim.ammon@gmail.com>
2022-12-08 15:38:06 +0100

    Reworked __init__.py according to: https://b3d.interplanety.org/en/creating-multifile-add-on-for-blender/

--- __init__.py
+++ __init__.py
@@ -12,20 +12,33 @@ bl_info = {
     'description': 'Adds additional object selection and deletion functionality.',
 }
 
-from . import ui
-from . import operator
+modulesNames = ['ui', 'operator']
 
-#####################################################################################
-# Add-On Handling
-#####################################################################################
+import sys
+import importlib
+
+modulesFullNames = {}
+for currentModuleName in modulesNames:
+    modulesFullNames[currentModuleName] = ('{}.{}'.format(__name__, currentModuleName))
+
+for currentModuleFullName in modulesFullNames.values():
+    if currentModuleFullName in sys.modules:
+        importlib.reload(sys.modules[currentModuleFullName])
+    else:
+        globals()[currentModuleFullName] = importlib.import_module(currentModuleFullName)
+        setattr(globals()[currentModuleFullName], 'modulesNames', modulesFullNames)
 
 def register():
-    ui.register()
-    operator.register()
+    for currentModuleName in modulesFullNames.values():
+        if currentModuleName in sys.modules:
+            if hasattr(sys.modules[currentModuleName], 'register'):
+                sys.modules[currentModuleName].register()
 
 def unregister():
-    ui.unregister()
-    operator.unregister()
-        
-if __name__ == '__main__':
+    for currentModuleName in modulesFullNames.values():
+        if currentModuleName in sys.modules:
+            if hasattr(sys.modules[currentModuleName], 'unregister'):
+                sys.modules[currentModuleName].unregister()
+
+if __name__ == "__main__":
     register()
