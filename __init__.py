26a0e1911d1514e5dbbaf4547a6d8adaea3d938a
AchimA <achim.ammon@gmail.com>
2022-12-09 10:55:21 +0100

    Minor version update 0.1.0 as this finally is the first releasable variant of this add-on.

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ import bpy
 bl_info = {
     "name": "CAD-Helper",
     "author": "A. R. Ammon",
-    "version": (0, 0, 5),
+    "version": (0, 1, 0),
     "blender": (3, 10, 0),
     "description": "Adds additional object selection and deletion functionality for hierarchically structured assemblies.",
     "warning": "",
