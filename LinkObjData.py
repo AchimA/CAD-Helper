dc2f6bc42559eb7feddabcbe69fb5340f526fab7
AchimA <achim.ammon@gmail.com>
2023-09-04 22:53:32 +0200

    Added Object Data Helper, fully functional!

--- LinkObjData.py
+++ LinkObjData.py
@@ -2,21 +2,149 @@
 
 import bpy
 import re
+
+from bpy.types import Context
 from . import shared_functions
 import tempfile
 import os
 
-linkable_objects = {}
+
+# Assign linkable collection.
+class LinkableCollectionItem(bpy.types.PropertyGroup):
+    name: bpy.props.StringProperty(name='Items Name', default='Unknown')
+    objects: bpy.props.CollectionProperty(type=bpy.types.PropertyGroup, name='Linkable Objects')
+    N_objects: bpy.props.IntProperty(name='Eumber of Objects', default=0)
+
+def ListIndexCallback(self, value):
+    # bpy.ops.generate_markers.change_marker() #other class function
+    
+    bpy.ops.object.list_select_collection()
+
+class LIST_OT_SelectCollection(bpy.types.Operator):
+    bl_idname = "object.list_select_collection"
+    bl_label = "Select Collection"
+
+    @classmethod
+    def poll(cls, context):
+        return context.scene.linkable_collections
+    
+    def execute(self, context):
+        linkable_collections = context.scene.linkable_collections
+        index = context.scene.lin_col_idx
+
+        name = linkable_collections[index].name
+        objects = [bpy.data.objects[o.name] for o in list(linkable_collections[index].objects)]
+
+        bpy.ops.object.select_all(action='DESELECT')
+        # bpy.context.view_layer.objects.active = None
+        for obj in objects:
+            obj.select_set(True)
+            bpy.context.view_layer.objects.active = obj
+
+        return{'FINISHED'}
+    
+class LIST_OT_LinkCollection(bpy.types.Operator):
+    bl_idname = "object.list_link_collection"
+    bl_label = "Link Collection"
+
+    @classmethod
+    def poll(cls, context):
+        return context.scene.linkable_collections
+    
+    def execute(self, context):
+        linkable_collections = context.scene.linkable_collections
+        index = context.scene.lin_col_idx
+
+        name = linkable_collections[index].name
+        bpy.ops.object.link_collections()
+        
+        context.scene.linkable_collections.remove(index)
+        context.scene.lin_col_idx = min(max(0, index-1), len(linkable_collections)-1)
+
+        return{'FINISHED'}
+
+
+class LIST_OT_LinkALLCollections(bpy.types.Operator):
+    bl_idname = "object.link_all_collections"
+    bl_label = "Link All Collections"
+
+    @classmethod
+    def poll(cls, context):
+        return context.scene.linkable_collections
+
+    def execute(self, context):
+        self.report({'INFO'}, "Liniking all collections'")
+        
+        linkable_collections = context.scene.linkable_collections
+        index = 0
+
+        while linkable_collections:
+            bpy.ops.object.link_collections()
+            
+            context.scene.linkable_collections.remove(index)
+            context.scene.lin_col_idx = min(max(0, index-1), len(linkable_collections)-1)
+
+        return {'FINISHED'}
+
+class LinkableCollection_UL_List(bpy.types.UIList):
+    """Demo UIList."""
+
+    def draw_item(self, context, layout, data, item, icon, active_data,
+                  active_propname, index):
+
+        # We could write some code to decide which icon to use here...
+        custom_icon = 'COLLECTION_COLOR_02'
+
+        # Make sure your code supports all 3 layout types
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            layout.label(text=f'{item.N_objects:4d}x   {item.name}', icon = custom_icon)
+            
+
+        elif self.layout_type in {'GRID'}:
+            layout.alignment = 'CENTER'
+            layout.label(text="", icon = custom_icon)
+
+class UIListPanelLinkableCollection(bpy.types.Panel):
+    """Creates a Panel in the Object properties window"""
+    bl_label = "CAD Object Data Helper"
+    bl_idname = "panel_linkable_collection"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'CAD Obj.Dat. Helper'
+    bl_context = 'objectmode'
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        obj = context.object
+
+        
+        row = layout.row()
+        row.operator(
+            'object.refresh_linkable_collection',
+            text=f'Rescan Selection',
+            icon='FILE_REFRESH'
+            )
+        
+        layout.template_list('LinkableCollection_UL_List', 'a list', scene, 'linkable_collections', scene, 'lin_col_idx')
+        
+        row = layout.row()
+        
+        row.operator(
+            'object.list_link_collection',
+            text='Link Collection',
+            icon='LINKED'
+            )
+        row.operator(
+            'object.link_all_collections',
+            text=f'Link all {len(context.scene.linkable_collections)} collections',
+            icon='LINKED'
+            )
 
 
 class RefreshLinkableCollection(bpy.types.Operator):
     '''
-    Links object data of objects with the same name.
-
-    I.e. if a selection contains ['bolt_M3, bolt_M3.001, bolt_M3.002, nut_M3, nut_M3.001] then;
-        - [bolt_M3, bolt_M3.001, bolt_M3.002] will be lined
-        and:
-        - [nut_M3, nut_M3.001] will be linked
+    Refresh the colletion of linkable objects
     '''
     bl_idname = 'object.refresh_linkable_collection'
     bl_label = 'Refresh Linkable Collection'
@@ -31,161 +159,66 @@ class RefreshLinkableCollection(bpy.types.Operator):
 
         unique_names = []
         for ob in objects:
+            print(ob.name)
             match = p.match(ob.name)
             match = match.group()
             if match not in unique_names:
                 unique_names.append(match)
         
-        global linkable_objects
-        linkable_objects = {}
-
+        bpy.context.scene.linkable_collections.clear()
         for u_name in unique_names:
             u_objects = []
             for ob in objects:
-                # print(ob.data)
-                if u_name in ob.name and ob.data not in u_objects:
-                    u_objects.append(ob)
+                # add object to list if another object with the same data is not already in the list
+                if u_name == p.match(ob.name).group():
+                    if ob.data not in [u.data for u in u_objects]:
+                        u_objects.append(ob)
             if len(u_objects) > 1:
-                linkable_objects[u_name] = u_objects
-        # bpy.ops.object.select_all(action='DESELECT')
-        return {'FINISHED'}
+                item = bpy.context.scene.linkable_collections.add()
+                item.name = u_name
+                for obj in u_objects:
+                    ob = item.objects.add()
+                    ob.name = obj.name
+                item.N_objects = len(u_objects)
+                # ToDo:
+                # Create Icon here...
 
-
-
-class LinkObjData(bpy.types.Operator):
-    '''
-    Links object data of objects with the same name.
-
-    I.e. if a selection contains ['bolt_M3, bolt_M3.001, bolt_M3.002, nut_M3, nut_M3.001] then;
-        - [bolt_M3, bolt_M3.001, bolt_M3.002] will be lined
-        and:
-        - [nut_M3, nut_M3.001] will be linked
-    '''
-    bl_idname = 'object.link_obj_data'
-    bl_label = 'Link Object Data'
-    bl_options = {"REGISTER", "UNDO"}
-
-    @classmethod
-    def poll(cls, context):
-        return context.selected_objects
-
-    def invoke(self, context, event):
-        bpy.ops.object.refresh_linkable_collection()
-        return self.execute(context)
-
-    def execute(self, context):
-        
-        return {'FINISHED'}
-    
-
-    def draw(self, context):
-        
-        layout = self.layout
-        # layout.use_property_split = True
-        layout.label(text='Linkable collections:')
-        layout.operator(
-            'object.refresh_linkable_collection',
-            text=f'Refresh collections!',
-            icon='FILE_REFRESH'
-            )
-        layout.operator(
-            'object.link_all_collections',
-            text=f'Link all {len(linkable_objects)} collections!',
-            icon='LINKED'
-            )
-        # layout.operator('object.update_linkable_collection', text='Refresh', icon='FILE_REFRESH')
-        for key, data in linkable_objects.items():
-            box = layout.box()
-            row_outer = box.row()
-            # box = row_outer.box()
-            # box.label(text='', icon='SNAP_FACE')
-            # icon_id = render_object_thumbnail(obj)
-            # row_outer.template_icon(icon_value=icon_id, scale=3.0)
-
-            box = row_outer.box()
-            box.label(text=f'[{len(data)}x] {key}', icon='OBJECT_DATA')
-
-            row = box.row()
-            row.operator(SelectCollections.bl_idname, text='Select', icon='RESTRICT_SELECT_OFF').collection_name = key
-            row.operator(LinkCollections.bl_idname, text='Link', icon='LINKED').collection_name = key
-            # row.label(text='Link Object Data', icon='LINKED')
-
-
-class SelectCollections(bpy.types.Operator):
-    bl_idname = "object.select_collections"
-    bl_label = "Select Collections"
-
-    collection_name: bpy.props.StringProperty()
-
-    def execute(self, context):
-        self.report({'INFO'}, f'Not yet implemented (sorry)')
-        # self.report({'INFO'}, f"Collection is '{self.collection_name}'")
-        # print(linkable_objects[self.collection_name])
-        
-        # bpy.ops.object.select_all(action='DESELECT')
-        # bpy.context.view_layer.objects.active = None
-        # for obj in linkable_objects[self.collection_name]:
-        #     print(obj)
-        #     obj.select_set(True)
-        #     bpy.context.view_layer.objects.active = obj
         return {'FINISHED'}
 
 class LinkCollections(bpy.types.Operator):
     bl_idname = "object.link_collections"
     bl_label = "Link Collections"
 
-    collection_name: bpy.props.StringProperty()
 
     def execute(self, context):
-        self.report({'INFO'}, f"Liniking: '{self.collection_name}'")
-
-        objects = linkable_objects[self.collection_name]
-        print(objects)
+        linkable_collections = context.scene.linkable_collections
+        index = context.scene.lin_col_idx
+        
+        self.report({'INFO'}, f"Liniking: '{linkable_collections[index].name}'")
+        
+        objects = [bpy.data.objects[o.name] for o in list(linkable_collections[index].objects)]
 
         first_object = objects.pop(0)
         
         for obj in objects:
-            print(f'{first_object.data} -> {obj.data}')
+            # print(f'{first_object.data} -> {obj.data}')
             obj.data = first_object.data
-        # remove from dict
-        linkable_objects.pop(self.collection_name, None)
-        return {'FINISHED'}
-
-class LinkALLCollections(bpy.types.Operator):
-    bl_idname = "object.link_all_collections"
-    bl_label = "Link All Collections"
-
-    def execute(self, context):
-        self.report({'INFO'}, "Liniking all collections'")
-        max_loops = 1e3
-        loop = 0
-        while linkable_objects and loop<max_loops:
-            key = list(linkable_objects.keys())[0]
-            print(key)
-            bpy.ops.object.link_collections(collection_name=key)
-            loop += 1
-            # object.link_collections().collection_name = key
-    #     objects = linkable_objects[self.collection_name]
-        # print(objects)
-
-    #     first_object = objects.pop(0)
         
-    #     for obj in objects:
-    #         print(f'{first_object.data} -> {obj.data}')
-    #         obj.data = first_object.data
-    #     # remove from dict
-    #     linkable_objects.pop(self.collection_name, None)
         return {'FINISHED'}
+
 
 ##############################################################################
 # Add-On Handling
 ##############################################################################
 __classes__ = (
-    LinkObjData,
     RefreshLinkableCollection,
-    SelectCollections,
+    LinkableCollectionItem,
+    LinkableCollection_UL_List,
+    UIListPanelLinkableCollection,
     LinkCollections,
-    LinkALLCollections,
+    LIST_OT_LinkALLCollections,
+    LIST_OT_SelectCollection,
+    LIST_OT_LinkCollection,
 )
 
 def register():
@@ -193,6 +226,10 @@ def register():
     for c in __classes__:
         bpy.utils.register_class(c)
         print(f'registered {c}')
+    
+    bpy.types.Scene.linkable_collections = bpy.props.CollectionProperty(type=LinkableCollectionItem)
+    # bpy.types.Scene.lin_col_idx = bpy.props.IntProperty(name='Index')
+    bpy.types.Scene.lin_col_idx = bpy.props.IntProperty(name='Index', update=ListIndexCallback)
 
 
 def unregister():
@@ -200,3 +237,5 @@ def unregister():
     for c in __classes__:
         bpy.utils.unregister_class(c)
         print(f'unregistered {c}')
+    del bpy.types.Scene.linkable_collections
+    del bpy.types.Scene.lin_col_idx
--- ui.py
+++ ui.py
@@ -159,69 +159,6 @@ class CAD_MAT_HELPER_PT_Panel(bpy.types.Panel):
             )
 
 
-class CAD_OBJ_DATA_LINKER_PT_Panel(bpy.types.Panel):
-    bl_idname = 'LINK_OBJ_DATA_PT_PANEL'
-    bl_label = 'CAD Object Data Helper'
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = 'CAD Helper'
-    bl_context = 'objectmode'
-
-    def draw(self, context):
-        layout = self.layout
-
-        box = layout.box()
-        box.label(text='Link Object Data (EXPERIMENTAL)')
-
-        box.operator_context = "INVOKE_DEFAULT"
-        box.operator(
-            'object.link_obj_data',
-            icon='LINKED'
-            )
-
-# class LINKABLE_OBJECTS_PT_Panel(bpy.types.Panel):
-#     bl_idname = 'Linkable_Objects_PT_Panel'
-#     bl_label = 'CAD Object Data Helper'
-#     bl_space_type = 'VIEW_3D'
-#     bl_region_type = 'UI'
-#     bl_category = 'CAD Object Linker'
-#     bl_context = 'objectmode'
-
-#     global linkable_objects
-
-#     def draw(self, context):
-#         layout = self.layout
-#         # layout.use_property_split = True
-#         layout.label(text='Linkable collections:')
-#         layout.operator('object.refresh_linkable_collection')
-#         # layout.operator('object.update_linkable_collection', text='Refresh', icon='FILE_REFRESH')
-#         for key, data in linkable_objects.items():
-#             # print(key)
-#             # print(data)
-#             # row = col.row()
-#             # row.label(text=key)
-            
-#             box = layout.box()
-            
-#             row_outer = box.row()
-            
-#             box = row_outer.box()
-#             box.label(text='Thumbnail')
-#             # icon_id = render_object_thumbnail(obj)
-#             # row_outer.template_icon(icon_value=icon_id, scale=3.0)
-
-#             box = row_outer.box()
-
-#             box.label(text=f'[{len(data)}x] {key}', icon='OBJECT_DATA')
-            
-#             row = box.row()
-            
-#             row.operator(SelectCollections.bl_idname, text='Select', icon='RESTRICT_SELECT_OFF').collection_name = key
-#             # op.custom_prop = key
-#             # row.label(text='Select Objects', icon='RESTRICT_SELECT_OFF')
-#             row.label(text='Link Object Data', icon='LINKED')
-   
-
 ##############################################################################
 # Add-On Handling
 ##############################################################################
@@ -230,8 +167,6 @@ __classes__ = (
     CAD_SEL_HELPER_PT_Panel,
     CAD_CLEAN_HELPER_PT_Panel,
     CAD_MAT_HELPER_PT_Panel,
-    CAD_OBJ_DATA_LINKER_PT_Panel,
-    # LINKABLE_OBJECTS_PT_Panel,
 )
 
 
